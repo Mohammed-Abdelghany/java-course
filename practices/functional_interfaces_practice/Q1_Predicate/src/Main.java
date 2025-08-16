400195720ecf8899327a924c5e4d07315ae7b62f
Mohammed-Abdelghany <muhammed195840@feng.bu.edu.eg>
2025-08-17 01:56:12 +0300

    Java Comparisons: List vs Vector, HashSet vs  LinkedHashSet, String vs StringBuilder vs  StringBuffe

--- practices/functional_interfaces_practice/Q1_Predicate/src/Main.java
+++ practices/functional_interfaces_practice/Q1_Predicate/src/Main.java
@@ -7,7 +7,6 @@ import java.util.function.Predicate;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
 //           Legacy code using anonymous class
 
 //        Scanner sc = new Scanner(System.in);
